29fcf21b9b6a24743232b4e5d907b75baa435d14
Shailendra Singh <srj0408@gmail.com>
2017-03-06 14:30:43 +0530

    added mysql DB creation

--- rpi_control.py
+++ rpi_control.py
@@ -1,4 +1,11 @@
 from flask import Flask , render_template, request
+import mysql.connector
+from mysql.connector import MySQLConnection, Error
+from python_mysql_dbconfig import read_db_config
+from mysql.connector import errorcode
+
+
+
 application = Flask(__name__)
 
 @application.route("/")
@@ -10,8 +17,11 @@ def tubelightstate():
     if request.method == 'POST': 
         tubelight_state_1 = request.form['toggle']
         print tubelight_state_1
+        create_machine_state_table() 
         if tubelight_state_1 == 'on':
             print "turning light on"
+            save_tubelight_state() #this function is still to be added in the to get the information of state of tubelight and save it in the mysql database
+
         else :
             print "turning light off"
         return tubelight_state_1
@@ -30,7 +40,7 @@ def fanstate():
 @application.route('/switch1state',methods = ['GET','POST'])
 def switch1state():
     if request.method == 'POST': 
-        switch1_state_1 = request.form['swicth1_state']
+        switch1_state_1 = request.form['switch1_state']
         print switch1_state_1
         if switch1_state_1 == 'on':
             print "turning Switch1 on"
@@ -47,8 +57,42 @@ def coffeemachine():
             print "turning coffee machine on"
         else :
             print "turning coffee machine off"
-    return None 
+    return coffeem_state_1 
           
+def create_machine_state_table():        
+    try:
+        db_config = read_db_config()
+        print db_config
+        print db_config['database']
+        conn = MySQLConnection(**db_config)
+        if conn.is_connected():
+            print('connection established.')
+        else:
+            print('connection failed.')
+
+        cursor = conn.cursor()
+        query ="""CREATE TABLE IF NOT EXISTS home_control (
+                  first_name CHAR(20) NOT NULL,
+                  last_name  CHAR(20),
+                  email_id VARCHAR(30),   
+                  contact_number VARCHAR(15),
+                  merchant_id int(11) NOT NULL AUTO_INCREMENT,
+                  tbubelight VARCHAR(15),
+                  fanstate VARCHAR(15),
+                  switch1state VARCHAR(15),
+                  coffeemachinestate VARCHAR(15),
+                  PRIMARY KEY (`merchant_id`) )""" 
+        cursor.execute(query)
+ 
+        conn.commit()
+
+    except Error as error:
+        print(error)
+        
+    finally:
+        cursor.close()
+        conn.close()
+        print ("connection closed.")
      
 
 if __name__ == "__main__":
