e348d74ae000f7292c9ecc1f56a2d2613434a712
Shailendra Singh <srj0408@gmail.com>
2017-03-08 03:17:59 +0530

    added a function to update the tubelight_state in the DB. Still need to add the same functionality for others. Also there is a bug that once you drop a table only tubelight function is able to generate the table if not exist

--- rpi_control.py
+++ rpi_control.py
@@ -15,16 +15,22 @@ def hello():
 @application.route('/tubelightstate',methods = ['GET','POST'])
 def tubelightstate():
     if request.method == 'POST': 
-        tubelight_state_1 = request.form['toggle']
-        print tubelight_state_1
+        global tubelight_state
+        global first_name 
+        first_name = "Shailendra"
+        tubelight_state = request.form['toggle']
+        print tubelight_state
         create_machine_state_table() 
-        if tubelight_state_1 == 'on':
+        if tubelight_state == 'on':
             print "turning light on"
-            save_tubelight_state() #this function is still to be added in the to get the information of state of tubelight and save it in the mysql database
+            save_tubelight_state(tubelight_state, first_name)
+            print "after saving on in the DB" 
 
         else :
             print "turning light off"
-        return tubelight_state_1
+            save_tubelight_state(tubelight_state, first_name) 
+            print "after saving off in the DB" 
+        return tubelight_state
 
 @application.route('/fanstate',methods = ['GET','POST'])
 def fanstate():
@@ -50,13 +56,16 @@ def switch1state():
 
 @application.route('/coffeemachine',methods = ['GET','POST'])
 def coffeemachine():
-    if request.method == 'POST': 
+    if request.method == 'POST':
+        first_name = "Shailendra" 
         coffeem_state_1 = request.form['coffeem_state']
         print coffeem_state_1
         if coffeem_state_1 == 'on':
             print "turning coffee machine on"
+            save_coffeem_state(first_name, coffeem_state_1)
         else :
             print "turning coffee machine off"
+            save_coffeem_state(first_name, coffeem_state_1)
     return coffeem_state_1 
           
 def create_machine_state_table():        
@@ -73,15 +82,10 @@ def create_machine_state_table():
         cursor = conn.cursor()
         query ="""CREATE TABLE IF NOT EXISTS home_control (
                   first_name CHAR(20) NOT NULL,
-                  last_name  CHAR(20),
-                  email_id VARCHAR(30),   
-                  contact_number VARCHAR(15),
-                  merchant_id int(11) NOT NULL AUTO_INCREMENT,
-                  tbubelight VARCHAR(15),
-                  fanstate VARCHAR(15),
-                  switch1state VARCHAR(15),
-                  coffeemachinestate VARCHAR(15),
-                  PRIMARY KEY (`merchant_id`) )""" 
+                  tubelight_state VARCHAR(20),
+                  fan_state_1 VARCHAR(20),
+                  switch1_state_1 VARCHAR(20),
+                  coffeem_state_1 VARCHAR(20) )""" 
         cursor.execute(query)
  
         conn.commit()
@@ -94,6 +98,35 @@ def create_machine_state_table():
         conn.close()
         print ("connection closed.")
      
+
+def save_tubelight_state(tubelight_state, first_name):
+    print first_name, tubelight_state
+    try:
+        db_config = read_db_config()
+        conn = MySQLConnection(**db_config)
+        if conn.is_connected():
+            print('connection established.')
+        else:
+            print('connection failed.')
+        
+        cursor = conn.cursor()
+        query ="""UPDATE home_control SET tubelight_state=%s WHERE first_name=%s"""
+        print query 
+        cursor.execute(query, (tubelight_state,first_name))
+        
+
+        conn.commit()
+        print "after commit"
+    except Error as error:
+        print(error)
+        
+    finally:
+        cursor.close()
+        conn.close()
+        print ("connection closed.")
+  
+
+ 
 
 if __name__ == "__main__":
     application.run(host='0.0.0.0')
