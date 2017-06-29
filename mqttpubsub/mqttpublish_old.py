ce541e661e6d9b3460dace03b5fd6d93108457de
Shailendra Singh <srj0408@gmail.com>
2017-06-29 13:11:59 +0530

    replaced the older version of cloudmqtt with hivemqtt

---
+++ mqttpubsub/mqttpublish_old.py
@@ -0,0 +1,40 @@
+import mosquitto, os, urlparse
+import time
+
+def on_connect(mosq, obj, rc):
+    print("rc: " + str(rc))
+
+def on_message(mosq, obj, msg):
+    print(msg.topic + " " + str(msg.qos) + " " + str(msg.payload))
+
+def on_publish(mosq, obj, mid):
+    print("mid: " + str(mid))
+
+def on_subscribe(mosq, obj, mid, granted_qos):
+    print("Subscribed: " + str(mid) + " " + str(granted_qos))
+
+def on_log(mosq, obj, level, string):
+    print(string)
+
+mqttc = mosquitto.Mosquitto()
+mqttc.on_message = on_message
+mqttc.on_connect = on_connect
+mqttc.on_publish = on_publish
+
+
+url_str = os.environ.get('m11.cloudmqtt:10167', 'mqtt://localhost:1883')
+url = urlparse.urlparse(url_str)
+
+
+def my_mqtt_publish(topic, payload):
+    mqttc.username_pw_set("wbjddnda", "oantKK07oLSg")
+    mqttc.connect(url.hostname, url.port)
+    try:
+        mqttc.publish(topic, payload)
+        print "going to publish topic : {0} and payload {1}".format(topic,payload)
+    except Exception as e:
+        print e.args, e.message
+
+
+
+
